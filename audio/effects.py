3998a997da8f3bbe66cfd1522f3537528ee8efcf
Nils J <nilsolejonack@gmail.com>
2023-12-30 16:53:03 +0100

    fixed distortion and reverb effect

--- audio/effects.py
+++ audio/effects.py
@@ -1,13 +1,15 @@
 from scipy.io import wavfile
+from scipy.signal import lfilter
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 # contains all functions that alter the recorded audio via an implemented effect
 WAVE_OUTPUT_FILENAME = ".wav"  # Output file extension
 INPUT_PATH = os.path.normpath(os.path.join(os.getcwd(), "Recordings"))
 OUTPUT_PATH = os.path.normpath(os.path.join(os.getcwd(), "Effects"))
 
-def tremolo_effect(input_filename, output_filename, rate, depth):
+def tremolo_effect(input_filename, output_filename, rate=4, depth=0.6):
     # Construct full paths
     input_path = os.path.join(INPUT_PATH, input_filename + WAVE_OUTPUT_FILENAME)
     output_path = os.path.join(OUTPUT_PATH, output_filename + WAVE_OUTPUT_FILENAME)
@@ -33,24 +35,40 @@ def tremolo_effect(input_filename, output_filename, rate, depth):
     print("Audio file saved to", output_path)
     return normalized_audio
 
-def distortion_effect(input_filename, output_filename, gain, clipping_threshhold):
+def distortion_effect(input_filename, output_filename, gain=1, clipping_threshold_percentage=0.4):
     # Construct full paths
     input_path = os.path.join(INPUT_PATH, input_filename + WAVE_OUTPUT_FILENAME)
     output_path = os.path.join(OUTPUT_PATH, output_filename + WAVE_OUTPUT_FILENAME)
 
-    # Load the wave file
+    # Load the audio file
     sample_rate, audio_data = wavfile.read(input_path)
 
-    # Apply gain
-    distorted_audio = audio_data * gain
-    # Apply clipping
-    distorted_audio = np.clip(distorted_audio, -clipping_threshhold, clipping_threshhold)
+    max_amplitude = np.max(np.abs(audio_data))
 
-    # Save the modified audio
+    # apply gain
+    amplified_audio = audio_data * gain
+
+    clipping_threshold = clipping_threshold_percentage * max_amplitude
+
+    distorted_audio = np.clip(amplified_audio, -clipping_threshold , clipping_threshold)
+
+    # Save the distorted audio
     wavfile.write(output_path, sample_rate, distorted_audio.astype(np.int16))
+
+    '''
+    # Plot the original and modified audio waveforms
+    plt.figure(figsize=(10, 4))
+    plt.plot(audio_data, label='Original')
+    plt.plot(distorted_audio, label='Distortion Effect')
+    plt.legend()
+    plt.title('Original and Modified Audio Waveforms')
+    plt.xlabel('Sample')
+    plt.ylabel('Amplitude')
+    plt.show()
+    '''
+
     print("Audio file saved to", output_path)
     return distorted_audio.astype(np.int16)
-
 
 def echo_effect(input_filename, output_filename, echo_delay_seconds=1, echo_amplifier=0.25):
     # Construct full paths
@@ -70,8 +88,7 @@ def echo_effect(input_filename, output_filename, echo_delay_seconds=1, echo_ampl
     print("Audio file saved to", output_path)
     return audio_data.astype(np.int16)
 
-
-def hall_effect(input_filename, output_filename, hall_delay_seconds=0.1, hall_length=0.5, hall_amplifier=0.5, hall_repeat=3):
+def reverb_effect(input_filename, output_filename, reverb_delay_ms=100, decay=0.5):
     # Construct full paths
     input_path = os.path.join(INPUT_PATH, input_filename + WAVE_OUTPUT_FILENAME)
     output_path = os.path.join(OUTPUT_PATH, output_filename + WAVE_OUTPUT_FILENAME)
@@ -79,26 +96,29 @@ def hall_effect(input_filename, output_filename, hall_delay_seconds=0.1, hall_le
     # Load the wave file
     sample_rate, audio_data = wavfile.read(input_path)
 
-    # Calculate variables
-    delay_samples = int(hall_delay_seconds * sample_rate)
-    repeat_delay_samples = int((hall_length / hall_repeat) * sample_rate)
-    sample_length = len(audio_data)
+    # Generate reverb impulse response with adjustable delay in milliseconds
+    delay_length = int(reverb_delay_ms * sample_rate / 1000)  # Convert milliseconds to samples
+    impulse_response = np.zeros(delay_length)
+    impulse_response[0] = 1.0
+    impulse_response[-1] = decay
 
-    # For every sample
-    for i in range(sample_length):
-        sample_position = i + delay_samples
-        count = 0
+    # Apply reverb using lfilter
+    reverb_signal = lfilter([1.0], impulse_response, audio_data)
 
-        while (sample_position < sample_length) & (count < hall_repeat):
-            # Add hall
-            repeat_amplifier = (hall_repeat - count) / hall_repeat
-            audio_data[sample_position] += (audio_data[i] * repeat_amplifier * hall_amplifier)
-
-            # Increase values
-            sample_position += repeat_delay_samples
-            count += 1
+    '''
+    # Plot the original and modified audio waveforms
+    plt.figure(figsize=(10, 4))
+    plt.plot(audio_data, label='Original')
+    plt.plot(reverb_signal, label='Reverb Effect')
+    plt.legend()
+    plt.title('Original and Modified Audio Waveforms')
+    plt.xlabel('Sample')
+    plt.ylabel('Amplitude')
+    plt.show()
+    '''
 
     # Save the modified audio
-    wavfile.write(output_path, sample_rate, audio_data.astype(np.int16))
+    wavfile.write(output_path, sample_rate, reverb_signal.astype(np.int16))
     print("Audio file saved to", output_path)
-    return audio_data.astype(np.int16)
+    return reverb_signal.astype(np.int16)
+
